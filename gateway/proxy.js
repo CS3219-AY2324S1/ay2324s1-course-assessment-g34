03c62b885896e27772ba2aa8761478e39ca1fec8
wweqg <e0726629@u.nus.edu>
2023-10-16 19:50:01 +0800

    fix

--- gateway/proxy.js
+++ gateway/proxy.js
@@ -12,11 +12,11 @@ const QUESTION_BASE_URL = process.env.QUESTION_URL;
 //const MATCHING_BASE_URL = process.env.MATCHING_URL;
 
 const userProxy = (req, res) => {
-  proxy.web(req, res, { target: HISTORY_BASE_URL });
+  proxy.web(req, res, { target: USER_BASE_URL });
 };
 
 const questionProxy = (req, res) => {
-    proxy.web(req, res, { target: VIDEO_BASE_URL });
+    proxy.web(req, res, { target: QUESTION_BASE_URL });
   };
 
 // const matchingWsProxy = createProxyMiddleware('/matching', {
