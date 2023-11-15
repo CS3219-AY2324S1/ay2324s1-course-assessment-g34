6ce7027ff54a16f7c1ebb21ae88bb8c3dafa86cf
izzahaj <izzah.jalil02@gmail.com>
2023-11-15 15:45:59 +0800

    Remove unused files and add route guard to collab page

--- frontend/src/pages/collab.jsx
+++ frontend/src/pages/collab.jsx
@@ -1,3 +1,5 @@
+import RouteGuard from '@/components/RouteGuard';
+import { Role } from '@/utils/constants';
 import { Skeleton } from '@mui/material';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
@@ -12,12 +14,14 @@ const DynamicCollabPage = dynamic(() => import('../components/CollabPage/CollabP
 export default function CollaborationPage() {
   return (
     <>
-      <Head>
-        <title>PeerPrep</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="PeerPrep is a technical interview preparation platform where students can find peers to practise interview questions together" />
-      </Head>
-      <DynamicCollabPage />
+      <RouteGuard allowedRoles={[Role.USER, Role.ADMIN]}>
+        <Head>
+          <title>PeerPrep</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content="PeerPrep is a technical interview preparation platform where students can find peers to practise interview questions together" />
+        </Head>
+        <DynamicCollabPage />
+      </RouteGuard>
     </>
   );
 }
